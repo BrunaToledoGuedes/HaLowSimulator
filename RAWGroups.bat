796b3df4acd351cafed0570395aaeca76c456075
BrunaToledoGuedes <brunatoledo30@gmail.com>
2025-09-30 16:02:39 -0300

    Update RAWGroups.bat

--- RAWGroups.bat
+++ RAWGroups.bat
@@ -1,2 +1,3 @@
-python simulator.py -n 200 -g 5 -s 1 -W 280 -H 280 -v 0 >\python39\Scripts\metrics\logArb.txt
-python SimParser.py -f logArb.txt >\python39\Scripts\metrics\RAW_200nodes5groups.txt
+python simulator.py -n 100 -g 5 -s 1 -W 280 -H 280 -v 0 >\python39\Scripts\metrics\logArb.txt
+
+python SimParser.py -f logArb.txt >\python39\Scripts\metrics\RAW_100nodes5groups.txt

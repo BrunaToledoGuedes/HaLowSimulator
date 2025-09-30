c321e5643a7bbe317514017211a2f738a1f31cf7
BrunaToledoGuedes <brunatoledo30@gmail.com>
2025-09-30 16:03:41 -0300

    Update customGroups.bat

--- customGroups.bat
+++ customGroups.bat
@@ -1,4 +1,4 @@
-python simulator.py -pE power.txt -n 200 -g 5 -s 1 -W 280 -H 280 -v 0 >\python39\Scripts\metrics\log.txt
-python RSCRAW.py -n 200 -g 5 -W 280 -H 280 -p power.txt -c coordinate.txt
-python simulator.py -pE p.txt -pP c.txt -n 200 -G groupFile.txt -g 5 -s 1 -W 280 -H 280 -v 0 >log2.txt
-python SimParser.py -f log2.txt >\python39\Scripts\metrics\RSCRAW_200nodes_5groups_s1.txt
+python simulator.py -pE power.txt -n 100 -g 5 -s 1 -W 280 -H 280 -v 0 >\python39\Scripts\metrics\log_RSCRAW.txt
+python RSCRAW.py -n 100 -g 5 -W 280 -H 280 -p power.txt -c coordinate.txt
+python simulator.py -pE p.txt -pP c.txt -n 100 -G groupFile.txt -g 5 -s 1 -W 280 -H 280 -v 0 >log.txt
+python SimParser.py -f log.txt >\python39\Scripts\metrics\RSCRAW_100nodes_5groups_s1.txt
